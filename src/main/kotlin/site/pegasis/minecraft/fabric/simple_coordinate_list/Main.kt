32609b0e3979355078e249f46cec2a95e527abbe
pegasis <me@pegasis.site>
2021-08-17 22:34:49 -0400

    init commit

---
+++ src/main/kotlin/site/pegasis/minecraft/fabric/simple_coordinate_list/Main.kt
@@ -0,0 +1,9 @@
+package site.pegasis.minecraft.fabric.simple_coordinate_list
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object Main: ModInitializer {
+    override fun onInitialize() {
+        println("simple_coordinate_list has been initialized.")
+    }
+}
